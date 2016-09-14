1d047ebd8ea797e6209dd83fa7e71c70eb1f35b1
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-09-14 14:48:52 +0300

    added if and for loop

---
+++ second.php
@@ -0,0 +1,36 @@
+<?php
+
+	$age = 5;
+	
+	if ($age < 18) {
+		
+		echo "alaealine";
+		
+	} else {
+		
+		echo "täisealine";
+		
+	}
+
+?>
+
+<br>
+
+<?php 
+
+	// 
+	for ($i = 1; $i <= $age; $i = $i + 1) {
+		
+		echo $i."palju ";
+		
+	}
+	
+	echo "õnne";
+
+
+?>
+
+
+
+
+
